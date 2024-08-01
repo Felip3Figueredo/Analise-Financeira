38222bc6768a5422b238772ecfe9c6a2ae51037b
Felipe Figueredo Dos Santos Aguiar <felipefigueredo1208@gmail.com>
2024-08-01 00:37:44 -0300

    Criando interceptador de requisições

--- Frontend/meu-financeiro/src/app/pages/login/login.component.ts
+++ Frontend/meu-financeiro/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent {
   
   constructor( public formBuilder: FormBuilder,
     private router: Router, 
-    private loginService:LoginService
+    private loginService:LoginService,
+    public authService: AuthService
   )
   {
 
@@ -37,11 +39,12 @@ export class LoginComponent {
   {
     this.loginService.login(this.dadosForm["email"].value, this.dadosForm["senha"].value).subscribe (
       token => {
-        alert(token);
-        this.router.navigate(['/dashboard'])
+        this.authService.setToken(token);
+        this.authService.UsuarioAutenticado(true);
+        this.router.navigate(['/dashboard']);
       },
       err => {
-        alert('Ocorreu um erro');
+        alert('Ocorreu um erro' + err);
       }
     )
   }
